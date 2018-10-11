cecc394144874a16220849c8b5ab7325167a498f
suryapraveen00 <suryapraveen95.adivi@gmail.com>
2018-10-11 10:49:14 +0530

    "check_style_score -> 5 of 5 passed. style score: 0/1 "

--- module5/5.1 Deque/Solution.java
+++ module5/5.1 Deque/Solution.java
@@ -17,17 +17,17 @@ class LinkedQueue {
 	}
 
 	boolean enqueue(int x) {
-		
-		//String elements ="";
-		
-		ll.add(++rear, x);
-		rear=ll.size()-1;
-		/*Iterator itr = ll.iterator();
-		while (itr.hasNext()) {
-			elements+=itr.next();
-			System.out.println("*"+elements);
 
-		}*/
+		// String elements ="";
+
+		ll.add(++rear, x);
+		rear = ll.size() - 1;
+		/*
+		 * Iterator itr = ll.iterator(); while (itr.hasNext()) { elements+=itr.next();
+		 * System.out.println("*"+elements);
+		 * 
+		 * }
+		 */
 		System.out.println(ll);
 		return true;
 
@@ -35,107 +35,110 @@ class LinkedQueue {
 
 	int dequeue() {
 
-		ll.remove(front + 1);
-		rear=ll.size()-1;
+		if (ll.isEmpty()) {
+			System.out.println("Deck is empty");
+		} else {
+			ll.remove(front + 1);
+			rear = ll.size() - 1;
 
-		//System.out.println("After removing from queue");
-		/*Iterator itr = ll.iterator();
+			// System.out.println("After removing from queue");
+			/*
+			 * Iterator itr = ll.iterator();
+			 * 
+			 * while (itr.hasNext()) { System.out.println(itr.next()); }
+			 */
+			System.out.println(ll);
 
-		while (itr.hasNext()) {
-			System.out.println(itr.next());
-		}*/
-		System.out.println(ll);
+		}
+
 		return 1;
+
 	}
 
 	int deleteEnd() {
-		
-		ll.remove(rear--);
-		rear=ll.size()-1;
-		//System.out.println("deleting elements from the end of queue");
-		//Iterator itr = ll.iterator();
 
-		/*while (itr.hasNext()) {
-			System.out.println(itr.next());
-		}*/
-		
-		System.out.println(ll);
+		if (ll.isEmpty()) {
+			System.out.println("Deck is empty");
+		} else {
+			ll.remove(rear--);
+			rear = ll.size() - 1;
+			// System.out.println("deleting elements from the end of queue");
+			// Iterator itr = ll.iterator();
+
+			/*
+			 * while (itr.hasNext()) { System.out.println(itr.next()); }
+			 */
+
+			System.out.println(ll);
+
+		}
 		return 1;
 
 	}
-	
+
 	int addFront(int z) {
-		
-		ll.add(front+1,z);
-		rear=ll.size()-1;
-		//System.out.println("Adding elements at the front of  queue");
-		/*Iterator itr = ll.iterator();
 
-		while (itr.hasNext()) {
-			System.out.println(itr.next());
-		}*/
+		ll.add(front + 1, z);
+		rear = ll.size() - 1;
+		// System.out.println("Adding elements at the front of queue");
+		/*
+		 * Iterator itr = ll.iterator();
+		 * 
+		 * while (itr.hasNext()) { System.out.println(itr.next()); }
+		 */
 		System.out.println(ll);
 		return 1;
-		
+
 	}
-	
-	
-	
-	
 
 }
 
-public class Solution{
+public class Solution {
 
 	public static void main(String[] args) {
- 		Scanner sc = new Scanner(System.in);
- 		LinkedQueue q = new LinkedQueue();
+		Scanner sc = new Scanner(System.in);
+		LinkedQueue q = new LinkedQueue();
 
- 		int operations =sc.nextInt();
- 		//sc.next();
- 		for(int i=1;i<=operations;i++) {
- 			//System.out.println("input - "+i);
- 			//sc.next();
- 			switch(sc.next()) {
- 			case "pushLeft"://q.enqueue(sc.nextInt());
- 				q.addFront(sc.nextInt());
- 			break;
- 			
- 			case "pushRight"://q.addFront(sc.nextInt());
- 				q.enqueue(sc.nextInt());
- 			break;
- 			
- 			case "popLeft":	q.dequeue();
- 			break;
- 			
- 			case "popRight":q.deleteEnd();
- 			break;
- 			
- 			case "isEmpty":System.out.println(q.ll.isEmpty());
- 			break;
- 			
- 			case "size":
- 				System.out.println(q.ll.size());
- 			
- 			break;
+		int operations = sc.nextInt();
+		// sc.next();
+		for (int i = 1; i <= operations; i++) {
+			// System.out.println("input - "+i);
+			// sc.next();
+			switch (sc.next()) {
+			case "pushLeft":// q.enqueue(sc.nextInt());
+				q.addFront(sc.nextInt());
+				break;
 
- 			}
- 		}
- 		
-		//LinkedQueue q = new LinkedQueue();
-		/*q.enqueue(10);
-		q.enqueue(20);
-		q.enqueue(30);
-		q.enqueue(40);
-		q.enqueue(50);
-		q.dequeue();
-		q.dequeue();
-		q.dequeue();
-		System.out.println("-------");
-		q.enqueue(30);
-		q.enqueue(40);
-		q.deleteEnd();
-		q.addFront(60);*/
+			case "pushRight":// q.addFront(sc.nextInt());
+				q.enqueue(sc.nextInt());
+				break;
+
+			case "popLeft":
+				q.dequeue();
+				break;
+
+			case "popRight":
+				q.deleteEnd();
+				break;
+
+			case "isEmpty":
+				System.out.println(q.ll.isEmpty());
+				break;
+
+			case "size":
+				System.out.println(q.ll.size());
+
+				break;
+
+			}
+		}
+
+		// LinkedQueue q = new LinkedQueue();
+		/*
+		 * q.enqueue(10); q.enqueue(20); q.enqueue(30); q.enqueue(40); q.enqueue(50);
+		 * q.dequeue(); q.dequeue(); q.dequeue(); System.out.println("-------");
+		 * q.enqueue(30); q.enqueue(40); q.deleteEnd(); q.addFront(60);
+		 */
 
 	}
 }
